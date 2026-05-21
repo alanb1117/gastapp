7da033831539c15045e01a5385e2ce69b9a91c04
Alan Bonandiez <93984694+alanb1117@users.noreply.github.com>
2026-05-20 22:54:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,6 @@
-const CACHE_NAME = 'gastapp-v17';
-const APP_SHELL = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
-});
-
+self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
-  event.respondWith(
-    fetch(req).catch(() => caches.match(req).then(resp => resp || caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch', event => { return; });
